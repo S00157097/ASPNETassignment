ac40bffbabcc0069dab1b01757dd9df756f5c474
Dima Savostijanovs <dsavostijanovs@yahoo.ie>
2016-03-04 16:42:44 +0000

    Added files via upload
    From School

---
+++ ASPNETassignment-master/Attempt1/index.aspx.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Attempt1
+{
+    public partial class index : Page
+    {
+        static string[] nationalities = new string[]
+        {
+            "Select Nationality", "American", "Armenian", "Australian", "Austrian", "Belgian", "Brazilian", "British", "Bulgarian", "Canadian",
+            "African", "Chinese", "Cuban", "Danish", "Dutch", "Egyptian", "Estonian", "Ethiopian", "Filipino", "Finnish", "French",
+            "German", "Greek", "Hungarian", "Indian", "Indonesian", "Iranian", "Iraqi", "Irish", "Italian", "Japanese", "Latvian", "Lithuanian",
+            "Mexican", "New Zealander", "Northern Irish", "Norwegian", "Polish", "Portuguese", "Romanian", "Russian", "Scottish", "Serbian",
+            "Slovakian", "South African", "Uruguayan", "Welsh"
+        };
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                lstNationality.DataSource = nationalities;
+                lstNationality.DataBind();
+            }
+        }
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            if (cbxRemember.Checked)
+            {
+                HttpCookie userData = new HttpCookie("userDataS00157097");
+                userData["FirstName"] = txtFirstName.Text;
+                userData["LastName"] = txtLastName.Text;
+                userData["Email"] = txtEmail.Text;
+                userData["Nationality"] = lstNationality.SelectedItem.Text;
+
+                userData.Expires = DateTime.Now.AddDays(1);
+                Response.Cookies.Add(userData);
+            }
+
+            Response.Redirect("quiz.aspx?q=1");
+        }
+    }
+}
